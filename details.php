79c573e6b1dbcea513ff8f50f5578a9e1a8b50bb
Luis Loreto <a216207287@unison.mx>
2023-04-25 14:41:34 -0700

    Footer categorías

--- details.php
+++ details.php
@@ -14,75 +14,83 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['submit'])) {
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['compare'])) {
 	$productId = $_POST['productId'];
     $insertCom = $pd->insertCompareData($productId,$cmrId);
-} ?> 
+} ?>
 
 <?php
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['wlist'])) {
     $saveWlist = $pd->saveWishListData($id,$cmrId);
-} ?> 
+} ?>
 
 <style>
-	.mybutton{width: 100px;float: left;margin-right: 50px;}
+.mybutton {
+    width: 100px;
+    float: left;
+    margin-right: 50px;
+}
 </style>
 <div class="main">
-	<div class="content">
-		<div class="section group">
-			<div class="cont-desc span_1_of_2">	
+    <div class="content">
+        <div class="section group">
+            <div class="cont-desc span_1_of_2">
 
-				<?php 
+                <?php 
 				$getPd = $pd->getSingleProduct($id);
 				if ($getPd) {
 					while ($result = $getPd->fetch_assoc()) { ?>
-						<div class="grid images_3_of_2">
-						<img src="admin/<?php echo $result['image']; ?>" alt="" />
-					</div>
-					<div class="desc span_3_of_2">
-						<h2><?php echo $result['productName']; ?> </h2>
-						<div class="price">
-							<p>Precio: <span>$ <?php echo $result['price']; ?></span></p>
-							<p>Categoria: <span><?php echo $result['catName']; ?></span></p>
-							<p>Marca: <span><?php echo $result['brandName']; ?></span></p>
-						</div>
-						<div class="add-cart">
-							<form action="" method="post">
-								<input type="number" class="buyfield" name="quantity" value="1"/>
-								<input type="submit" class="buysubmit" name="submit" value="Agregar al carrito"/>
-							</form>				
-						</div>
-						<span style="color: red;font-size: 18px;">
+                <div class="grid images_3_of_2">
+                    <img src="admin/<?php echo $result['image']; ?>" alt="" />
+                </div>
+                <div class="desc span_3_of_2">
+                    <h2><?php echo $result['productName']; ?> </h2>
+                    <div class="price">
+                        <p>Precio: <span>$ <?php echo $result['price']; ?></span></p>
+                        <p>Categoria: <span><?php echo $result['catName']; ?></span></p>
+                        <p>Marca: <span><?php echo $result['brandName']; ?></span></p>
+                    </div>
+                    <div class="add-cart">
+                        <form action="" method="post">
+                            <input type="number" class="buyfield" name="quantity" value="1" />
+                            <input type="submit" class="buysubmit" name="submit" value="Agregar al carrito" />
+                        </form>
+                    </div>
+                    <span style="color: red;font-size: 18px;">
 
-							<?php 
+                        <?php 
 							if (isset($addCart)) {
 								echo $addCart;
 							} ?>
-							<?php 
+                        <?php 
 							if (isset($insertCom)) {
 								echo $insertCom;
 							}
 							if (isset($saveWlist)) {
 								echo $saveWlist;
 							} ?>
-								
-						</span>
-					</div>
-					<div class="product-desc">
-						<h2>DETALLES DEL PRODUCTO</h2>
-						<?php echo $result['body']; ?>
-					</div>
-				<?php } } ?>
-			</div>
-			<div class="rightsidebar span_3_of_1">
-				<h2>CATEGORÍAS</h2>
-				<ul>
-					<?php 
+
+                    </span>
+                </div>
+                <div class="product-desc">
+                    <h2>DETALLES DEL PRODUCTO</h2>
+                    <?php echo $result['body']; ?>
+                </div>
+                <?php } } ?>
+            </div>
+
+            <div class="rightsidebar span_3_of_1">
+                <h2>CATEGORÍAS</h2>
+                <ul>
+                    <?php 
 					$getCat = $cat->getAllCat();
 					if ($getCat) {
 						while ($result = $getCat->fetch_assoc()) { ?>
-							<li><a href="productbycat.php?catId=<?php echo $result['catId']; ?>"><?php echo $result['catName']; ?></a></li>
-						<?php }} ?>
-					</ul>
-				</div>
-			</div>
-		</div>
-	</div>
-  <?php include 'inc/footer.php';?>
+                    <li><a
+                            href="productbycat.php?catId=<?php echo $result['catId']; ?>"><?php echo $result['catName']; ?></a>
+                    </li>
+                    <?php }} ?>
+                </ul>
+            </div>
+
+        </div>
+    </div>
+</div>
+<?php include 'inc/footer.php';?>
--- inc/footer.php
+++ inc/footer.php
@@ -1,65 +1,71 @@
 </div>
-   <div class="footer">
-   	<div class="wrapper">	
-	    <div class="section group">
-		<div class="col_1_of_4 span_1_of_4">
-						<h4>información</h4>
-						<ul>
-						<li><a href="#">Sobre Nosotros</a></li>
-						<li><a href="#">Contactanos</a></li>
-						</ul>
-					</div>
-				<div class="col_1_of_4 span_1_of_4">
-					<h4>Por qué comprar con nosotros</h4>
-						<ul>
-						<li><a href="#">Sobre Nosotros</a></li>
-						<li><a href="#">Política de privacidad</a></li>
-						</ul>
-				</div>
-				<div class="col_1_of_4 span_1_of_4">
-					<h4>Mi cuenta</h4>
-						<ul>
-							<li><a href="#">Iniciar sesión</a></li>
-							<li><a href="#">Ver carrito</a></li>
-							<li><a href="#">Ayuda</a></li>
-						</ul>
-				</div>
-				<div class="col_1_of_4 span_1_of_4">
-					<h4>Contactanos</h4>
-						<ul>
-							<li><span>+00-01234567899</span></li>
-							<li><span>micarrito@email.com</span></li>
-						</ul>
-						<div class="social-icons">
-							<h4>Síguenos</h4>
-					   		  <ul>
-							      <li class="facebook"><a href="#" target="_blank"> </a></li>
-							      <li class="twitter"><a href="#" target="_blank"> </a></li>
-							      <div class="clear"></div>
-						     </ul>
-   	 					</div>
-				</div>
-			</div>
-			<div class="copy_right">
-				<p>Carrito online shopping &amp; All rights Reseverd </p>
-		   </div>
-		</div>
-	</div>
-	<link href="css/flexslider.css" rel='stylesheet' type='text/css' />
-	<script defer src="js/jquery.flexslider.js"></script>
-	<script type="text/javascript">
-		$(function(){
-			SyntaxHighlighter.all();
-		});
-		$(window).load(function(){
-			$('.flexslider').flexslider({
-				animation: "slide",
-				start: function(slider){
-					$('body').removeClass('loading');
-				}
-			});
-		});
-	</script>
+<div class="footer">
+    <div class="wrapper">
+        <div class="section group">
+            <div class="col_1_of_4 span_1_of_4">
+                <h4>Información</h4>
+                <ul>
+                    <li><a href="#">Sobre Nosotros</a></li>
+                    <li><a href="#">Contactanos</a></li>
+                </ul>
+            </div>
+            <div class="col_1_of_4 span_1_of_4">
+                <h4>Por qué comprar con nosotros</h4>
+                <ul>
+                    <li><a href="#">Sobre Nosotros</a></li>
+                    <li><a href="#">Política de privacidad</a></li>
+                </ul>
+            </div>
+            <div class="col_1_of_4 span_1_of_4">
+                <h4>Categorías</h4>
+                <ul>
+                    <?php 
+					$getCat = $cat->getAllCat();
+					if ($getCat) {
+						while ($result = $getCat->fetch_assoc()) { ?>
+                    <li><a
+                            href="productbycat.php?catId=<?php echo $result['catId']; ?>"><?php echo $result['catName']; ?></a>
+                    </li>
+                    <?php }} ?>
+                </ul>
+            </div>
+            <div class="col_1_of_4 span_1_of_4">
+                <h4>Contactanos</h4>
+                <ul>
+                    <li><span>+00-01234567899</span></li>
+                    <li><span>micarrito@email.com</span></li>
+                </ul>
+                <div class="social-icons">
+                    <h4>Síguenos</h4>
+                    <ul>
+                        <li class="facebook"><a href="#" target="_blank"> </a></li>
+                        <li class="twitter"><a href="#" target="_blank"> </a></li>
+                        <div class="clear"></div>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="copy_right">
+            <p>Carrito online shopping &amp; All rights Reseverd </p>
+        </div>
+    </div>
+</div>
+<link href="css/flexslider.css" rel='stylesheet' type='text/css' />
+<script defer src="js/jquery.flexslider.js"></script>
+<script type="text/javascript">
+$(function() {
+    SyntaxHighlighter.all();
+});
+$(window).load(function() {
+    $('.flexslider').flexslider({
+        animation: "slide",
+        start: function(slider) {
+            $('body').removeClass('loading');
+        }
+    });
+});
+</script>
 
 </body>
+
 </html>
